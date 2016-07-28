78e2d582b950d881d6a56b1399beef87f4c146d8
Nicholas Gallardo <nic.gallardo@gmail.com>
2016-07-27 21:04:25 -0600

    initial commit -m

---
+++ index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+// render(<App/>, document.getElementById('app'))
+
+import { Router, Route, hashHistory } from 'react-router'
+
+import App from './modules/App'
+import About from './modules/About'
+import Repos from './modules/Repos'
+
+
+render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+    <Route path="/repos" component={Repos}/>
+    <Route path="/about" component={About}/>
+  </Router>
+), document.getElementById('app'))
+
+
+// render((
+//   <Router history={hashHistory}>
+//     // <Route path="/" component={App}/>
+//     {/* add the routes here */}
+//   </Router>
+// ), document.getElementById('app'))
