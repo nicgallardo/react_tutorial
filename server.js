b6f52e68bc77db69affdd03b13b4c7ed5344cd20
Nicholas Gallardo <nic.gallardo@gmail.com>
2016-08-19 23:56:30 -0600

    initial

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ function renderPage(appHtml) {
     <!doctype html public="storage">
     <html>
     <meta charset=utf-8/>
-    <title>My First React Router App</title>
+    <title>Nic Gallardo - Developer</title>
     <link rel=stylesheet href=/index.css>
     <div id=app>${appHtml}</div>
     <script src="/bundle.js"></script>
